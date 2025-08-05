86174a68c82641a166f59f56ef37b9db33d8f77f
Daniel Engval <daniel@engvalls.eu>
2025-08-05 09:32:48 +0200

    Before restructuring so that download PDF after expansion
    Refactor `scrape.py`: update default login timeout, add reusable `pause` method, enhance PDF invoice download logic, and improve logging clarity.

--- src/kvarnen_electricity/scrape.py
+++ src/kvarnen_electricity/scrape.py
@@ -3,7 +3,7 @@ from .log import logger
 
 
 class Scraper:
-    def __init__(self, login_secs: int = 30):
+    def __init__(self, login_secs: int = 20):
         self.login_secs = login_secs
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.firefox.launch(headless=False)
@@ -20,6 +20,10 @@ class Scraper:
             _scroll_count += 1
             logger.info(f"scrolling to bottom - {_scroll_count / _max_scrolls:.0%}")
 
+    @staticmethod
+    def pause(page, time_ms=1000):
+        page.wait_for_timeout(time_ms)
+
     def run(self) -> None:
         page = self.context.new_page()
         # page.goto("https://idp.jonkopingenergi.se/Account/BankID?returnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dweb-MinaSidor%26redirect_uri%3Dhttps%253A%252F%252Fminasidor.jonkopingenergi.se%252Fsignin-oidc%26response_type%3Dcode%26scope%3Dopenid%2520offline_access%26state%3Dc12c916190eb4d56b92003ac10be6318%26code_challenge%3D6UYUzudewlsAPQIcu2mcs4Hu3iDe-cNNXbmgkbaVOAk%26code_challenge_method%3DS256%26type%3Dprivate")
@@ -28,26 +32,33 @@ class Scraper:
         page.get_by_role("button", name="BankID logga BankID med QR-kod").click()
         for time_elapsed in range(self.login_secs):
             logger.info(f"Waiting for you to login using BankID on your device - {self.login_secs - time_elapsed} seconds left ...")
-            page.wait_for_timeout(1000)
+            self.pause(page)
         page.goto("https://minasidor.jonkopingenergi.se/")
         expect(page.get_by_role("link", name="Fakturor", exact=True)).to_be_visible()
         page.get_by_role("link", name="Fakturor", exact=True).click()
-        page.wait_for_timeout(2000)
+        self.pause(page, 10000)
+        page.get_by_text('Daniel Engvall', exact=True).all()[1].click()
+        self.pause(page)
         self.scroll_to_bottom(page)
-        for idx, month in enumerate(['juni', 'maj']):
-            page.get_by_role("button", name=f"{month} 2025").click()
-            page.wait_for_timeout(2000)
+        all_rows = page.get_by_text('Daniel Engvall', exact=True).all()
+        for idx, r in enumerate(all_rows):
+            logger.info(f"clicking on row {idx} of {len(all_rows)}")
+            r.click()
+            self.pause(page)
+        all_pdf_buttons = page.get_by_text('Visa PDF-faktura', exact=True).all()
+        for idx, pdf_button in enumerate(all_pdf_buttons[1:]):
             with page.expect_download() as download_info:
-                page.get_by_role("button", name="Visa PDF-faktura").nth(idx).click()
+                pdf_button.scroll_into_view_if_needed()
+                pdf_button.click()
             download = download_info.value
-            download.save_as(f"/Users/edo/Downloads/faktura_{month}.pdf")
-            page.wait_for_timeout(2000)
+            download.save_as(f"/Users/edo/Downloads/faktura_{idx}.pdf")
+            self.pause(page)
         logger.info('Done')
 
         page.get_by_role("button", name="Öppna profil").click()
-        page.wait_for_timeout(1000)
+        self.pause(page)
         page.get_by_role("button", name="Logga ut").click()
-        page.wait_for_timeout(1000)
+        self.pause(page)
         page.close()
 
     def close(self):
