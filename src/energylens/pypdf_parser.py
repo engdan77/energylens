da1caaa395348e7d5620c47c5d60fd93f5cd9d91
Daniel Engval <daniel@engvalls.eu>
2025-08-08 15:20:41 +0200

    Refine regex patterns for improved extraction, enhance robustness with case-insensitivity and optional matches.

--- src/energylens/pypdf_parser.py
+++ src/energylens/pypdf_parser.py
@@ -20,23 +20,22 @@ def _pdf_to_text(pdf_file: Path) -> list[str]:
 def _texts_to_pl(text_pages: list[str]) -> pl.DataFrame:
     text = " ".join(text_pages)
     d = {}
-    d['Elnät fast avgift enkeltariff (kr/mån)'] = re.findall(r'^.+?Fast avgift enkeltariff.*?(\d+,\d{2})', text,
-                                                             re.DOTALL | re.IGNORECASE)
-    d['El förbrukning (kWh)'] = re.findall(r'(\d+,\d{2}) kWh', text)
-    d['Elnät överföring enkeltariff (öre/kWh)'] = re.findall(r'Överföring enkeltariff\s+\d+,\d{2}(\d+,\d{2})', text)
+    d['Elnät fast avgift enkeltariff (kr/mån)'] = re.findall(r'^.+?Fast avgift enkeltariff.*?(\d+,\d{2})', text, re.DOTALL | re.IGNORECASE)
+    d['El förbrukning (kWh)'] = re.findall(r'(\d+(?:,\d{1,3})?) kWh\s', text, re.IGNORECASE)
+    d['Elnät överföring enkeltariff (öre/kWh)'] = re.findall(r'Överföring(?: enkeltariff)?\s+\d+,\d{2}(\d+,\d{2})', text)
     d['Elnät energiskatt (öre/kWh)'] = re.findall(r'Energiskatt.+\d+,\d{2}(\d+,\d{2})', text)
     d['Elnät totalt belopp (kr)'] = re.findall(r'TOTALT BELOPP ELNÄT.+?(\d+,\d{2}) kr\n', text)
     d['Elhandel medelspotpris (öre/kWh)'] = np.nan
-    d['Elhandel rörliga kostnader (öre/kWh)'] = re.findall(r'Rörligt månadspris.+?\d+,\d{2}(\d+,\d{2})', text)
+    d['Elhandel rörliga kostnader (öre/kWh)'] = re.findall(r'(?:Rörligt månadspris|Elpris).+?\d+,\d{2}(\d+,\d{2})', text)
     d['Elhandel fasta påslag (öre/kWh)'] = np.nan
     d['Elhandel fasta avgift (kr/mån)'] = re.findall(
         r'TOTALT BELOPP ELNÄT.+?ELHANDEL\n.+?kr/mån.+?Fast avgift.+?\d+,\d{2}(\d+,\d{2})', text, re.DOTALL)
     d['Elhandel totalt belopp (kr)'] = re.findall(r'ELHANDEL (\d+,\d{2}) kr', text)
-    d['Fjärrvärme förbrukning (MWh)'] = re.findall(r'(\d+,\d{1,2}) MW', text)
-    d['Fjärrvärme fast avgift (kr/år)'] = re.findall(r'\d+ dgr kr/år krFast Avgift\s+\d+,\d{2}([\d\s]+,\d{2})', text)
+    d['Fjärrvärme förbrukning (MWh)'] = re.findall(r'(\d+,\d{1,3}) MW', text)
+    d['Fjärrvärme fast avgift (kr/år)'] = re.findall(r'\d+ dgr kr/år krFast [Aa]vgift\s+.+\d+,\d{2}([\d\s]+,\d{2})', text, re.IGNORECASE)
     d['Fjärrvärme energiavgift (kr/MWh)'] = re.findall(r'kr/MWh krEnergiavgift\s+[\d+\s]+,\d{2}(\d+,\d{2})', text)
     d['Fjärrvärme totalt belopp (kr)'] = re.findall(r'FJÄRRVÄRME ([\d+\s]+,\d{2}) kr', text)
-    d['Stadsnät serviceavgift villa (kr/st)'] = re.findall(r'Serviceavgift villa.+?\d+,\d{2}(\d+,\d{2})', text)
+    d['Stadsnät serviceavgift villa (kr/st)'] = re.findall(r'Serviceavgift [Vv]illa.+?\d+,\d{2}(\d+,\d{2})', text)
     date = da[0] if (da := re.findall(r'\d{4}-\d{2}-\d{2}', text)) else np.nan
     logger.info(f'Date: {date} using PyPDF')
     invoice_number = i[0] if (i := re.findall(r'Faktura-nr: (\d+)', text)) else np.nan
